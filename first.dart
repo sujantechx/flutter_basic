9482aad70636d815c6f3b66e5740a90b072f14ae
Sujan <sujansahu655@gmail.com>
2025-04-30 09:45:19 +0530

    first dart

---
+++ first.dart
@@ -0,0 +1,10 @@
+ import 'dart:io';
+
+void main(){
+  stdout.write("Hello wlcm ");
+  print("Sujan");
+
+  print("Enter year name:");
+  String? name =stdin.readLineSync()!;
+  print("hello $name");
+ }
