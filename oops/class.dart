fe3cbbd359b3644b20837c899a598a801c515f91
Sujan <sujansahu655@gmail.com>
2025-04-30 09:45:36 +0530

    class

---
+++ oops/class.dart
@@ -0,0 +1,18 @@
+ void main(){
+  // Human a=Human('sujan',22);
+  Human b=Human();
+
+
+ }
+
+
+ class Human{
+String  ?name;
+int ?age;
+/*
+///parameter constructere
+ Human(this.name,this.age){
+   //ont block
+ }
+*/
+ }
